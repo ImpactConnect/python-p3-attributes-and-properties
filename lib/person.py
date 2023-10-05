b79504fa41d6f9709ffb913bf49daeae42820811
Omotayo <kehindeomotayo11@gmail.com>
2023-10-05 09:54:46 +0100

    Done

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -15,5 +17,38 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+
+    def __init__(self, name, job):
+        self._name = name  
+        self._job = job  
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) <= 25:
+            self._name = value.title()  
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in self.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
+
+person = Person("john doe", "Engineer")
+print(person.name)  
+print(person.job)  
+
+person.name = "Buddy The Super Amazing Fantastic Person With a Very Long Name"
+
+person.job = "Driver"
+
